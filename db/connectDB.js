6879631974be782ea94eb61ac54030a5af291499
Nikhil sharm <purohitwork2002@gmail.com>
2024-11-28 00:23:34 +0530

    five

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURL=mongoose.connect("mongodb://127.0.0.1:27017/admissionPortal")
+//const mongoURL=mongoose.connect("mongodb://127.0.0.1:27017/admissionPortal")
+const mongoURL=mongoose.connect("mongodb+srv://purohitwork2002:n9713860886@cluster0.lq5ke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const connectDB = () => {
   return mongoURL
 
